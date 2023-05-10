964e5e1ada82126d8fc040b19ce2b5b60d18507b
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2023-05-10 22:24:36 +0200

    Optimized SchemaFilesReader

--- jsonschema-to-mermaid/src/main/kotlin/com/github/flemming_n_larsen/mermaid_class_diagram_generator/SchemaFilesReader.kt
+++ jsonschema-to-mermaid/src/main/kotlin/com/github/flemming_n_larsen/mermaid_class_diagram_generator/SchemaFilesReader.kt
@@ -3,7 +3,6 @@ package com.github.flemming_n_larsen.mermaid_class_diagram_generator
 import com.google.gson.GsonBuilder
 import org.yaml.snakeyaml.Yaml
 import java.io.FileReader
-import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.isDirectory
@@ -14,32 +13,23 @@ object SchemaFilesReader {
     private val gson = GsonBuilder().create()
     private val yaml = Yaml()
 
-    fun readSchemas(source: Set<Path>): Collection<Schema> {
-        val files = mutableSetOf<Path>()
-        collectAllFiles(source, files)
+    fun readSchemas(source: Set<Path>): Collection<Schema> =
+        collectAllFiles(source).map { readSchema(it) }
 
-        val schemas = mutableListOf<Schema>()
-        files.forEach {
-            schemas.add(readSchema(it))
-        }
-        return schemas
-    }
-
-    private fun collectAllFiles(source: Set<Path>, fileSetOut: MutableSet<Path>) {
-        source.forEach { file ->
+    private fun collectAllFiles(source: Set<Path>): Set<Path> =
+        source.flatMap { file ->
             if (file.isDirectory()) {
-                collectAllFiles(Files.list(file).toList().toSet(), fileSetOut)
+                collectAllFiles(Files.list(file).toList().toSet())
             } else {
-                fileSetOut.add(file)
+                setOf(file)
             }
-        }
-    }
+        }.toSet()
 
     private fun readSchema(path: Path): Schema =
         when (getFileExtension(path)) {
             "json" -> readJsonSchema(path)
             "yaml", "yml" -> readYamlSchema(path)
-            else -> throw IOException("Unsupported file type: " + path.fileName.toString())
+            else -> throw UnsupportedOperationException("Unsupported file type: ${path.fileName}")
         }
 
     private fun getFileExtension(path: Path): String {
@@ -49,15 +39,19 @@ object SchemaFilesReader {
     }
 
     private fun readJsonSchema(path: Path): Schema {
-        val schema = gson.fromJson(FileReader(path.toFile()), Schema::class.java)
-        if (schema.dollarId.isNullOrBlank()) {
-            schema.dollarId = path.fileName.toString()
+        FileReader(path.toFile()).use { fileReader ->
+            val schema = gson.fromJson(fileReader, Schema::class.java)
+            if (schema.dollarId.isNullOrBlank()) {
+                schema.dollarId = path.fileName.toString()
+            }
+            return schema
         }
-        return schema
     }
 
     private fun readYamlSchema(path: Path): Schema {
-        val yaml: Map<String, Any> = yaml.load(FileReader(path.toFile()))
-        return gson.fromJson(gson.toJson(yaml), Schema::class.java)
+        FileReader(path.toFile()).use { fileReader ->
+            val yaml: Map<String, Any> = yaml.load(fileReader)
+            return gson.fromJson(gson.toJson(yaml), Schema::class.java)
+        }
     }
 }
